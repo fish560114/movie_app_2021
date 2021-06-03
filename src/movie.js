804c9786c8257db6a5216914d638fa11320e6be7
DESKTOP-7M5IVBI\Admin <fish560114@gmail.com>
2021-06-03 17:28:08 +0900

    4강 에러 3강부터 다시

--- src/movie.js
+++ src/movie.js
@@ -1,17 +1,14 @@
-//실제로 할일 axios를 통해 받은 movie 데이터를 렌더링하기.
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}){
-    return <h4>{title}</h4>
-}
 
 Movie.propTypes = {
-    id: PropTypes.number.isRequired,
-    year: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-}
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+};
 
-export default Movie
+export default Movie;
